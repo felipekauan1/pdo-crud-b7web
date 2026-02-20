ad720c590d3b3b9080370f4a74003a4da57a3a33
Felipe Kauãn <felipekauan30@outlook.com>
2026-02-20 18:19:41 -0300

    feat: implementa atualização de dados de usuários (Update)

---
+++ update.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'connection.php';  // para ter acesso a $pdo
+
+// cria uma query de valores como parametros
+$sql = "UPDATE usuarios SET user_name = :user_name WHERE id = :id";
+
+// prepara a statement
+$stmt = $pdo->prepare($sql);
+
+// cria variáveis com os valores
+$_user_name = 'luzin';
+$_id = 3;
+
+// substitui os parametros da query por valores
+$stmt->bindParam(':user_name', $_user_name);
+$stmt->bindParam(':id', $_id);
+
+// tenta executar a query no banco de dados
+try {
+    $stmt->execute();
+    echo 'Usuário atualizado com sucesso<br>';
+    echo $stmt->rowCount(). 'Linhas afetadas';
+} catch (Exception $e) {
+    echo 'Erro: ' . $e;
+}
+?>
